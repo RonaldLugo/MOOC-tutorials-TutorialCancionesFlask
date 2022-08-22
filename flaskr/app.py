3789d4d4274d67cd91df137db7b6603b464e3c1f
RonaldLugo <r.lugoq@uniandes.edu.co>
2022-08-21 21:44:12 -0500

    Update app.py

--- flaskr/app.py
+++ flaskr/app.py
@@ -18,14 +18,11 @@ db.create_all()
     print(Cancion.query.all()) """
 
 #Prueba 2
-with app.app_context():
+""" with app.app_context():
     u = Usuario(nombre_usuario='Juan', contrasena='12345')
     a = Album(titulo='prueba', anio=1999, descripcion='texto', medio=Medio.CD)
-    #c = Cancion(titulo='mi cancion', minutos=1, segundos=15, interprete='Cantante')
     u.albumes.append(a)
-    #a.canciones.append(c)
     db.session.add(u)
-    #db.session.add(c)
     db.session.commit()
     # validar albumes vs usuarios
     # ver usuarios y los albumes del primer usuario
@@ -36,4 +33,29 @@ with app.app_context():
     # ya no hay usuarios ni albumes (notar que se consulta la clase Album y no la lista de albumes de usuario)
     print(Usuario.query.all())
     print(Album.query.all())
+    print("----------")
+    db.session.commit() """
+
+#Prueba 3
+with app.app_context():
+    u = Usuario(nombre_usuario='Juan', contrasena='12345')
+    a = Album(titulo='prueba', anio=1999, descripcion='texto', medio=Medio.CD)
+    c = Cancion(titulo='mi cancion', minutos=1, segundos=15, interprete='Cantante')
+    u.albumes.append(a)
+    a.canciones.append(c)
+    db.session.add(u)
+    db.session.add(c)
     db.session.commit()
+    # validar albums vs canciones
+    # ver albumes, canciones asociadas al primer album y todas las canciones
+    print(Album.query.all())
+    print(Album.query.all()[0].canciones)
+    print(Cancion.query.all())
+    # al borrar un album, las canciones relacionadas siguen existiendo
+    db.session.delete(a)
+    # ya no hay albumes, pero siguen existiendo las canciones
+    print(Album.query.all())
+    print(Cancion.query.all())
+    db.session.commit()
+
+
